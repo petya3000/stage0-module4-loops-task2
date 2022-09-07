0914c738b57b499008867e73651eab154e7ba764
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-07 18:06:10 +0300

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int num = 0;
+
+            while (num <=  multiplyByAndToInclusive){
+                System.out.println(num *  multiplyByAndToInclusive);
+                num++;
+            }
     }
 }
