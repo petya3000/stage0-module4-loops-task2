7b1e8d544d0431ea751bc1e4a24fa4cc58c6b848
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-07 18:03:49 +0300

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int num = 0;
+
+            while (num <= lastPrinted){
+                System.out.println(num);
+                num++;
+            }
     }
 }
