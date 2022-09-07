97dac072c459a24b870d891709e82df317c470f2
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-07 18:09:44 +0300

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int deg = 0;
+
+            while (deg <= power){
+                System.out.println(Math.pow(2, deg));
+                deg++;
+            }
 
     }
 }
