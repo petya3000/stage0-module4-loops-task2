0c2a43ddf2e410c2468ca02027603b9c4340b4c2
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-07 18:07:14 +0300

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int num = 3;
+            if (printToInclusive < 2) return;
+            boolean flag = true;
+            System.out.println(2);
+            while (num <= printToInclusive){
+                for (int i = 2; i < num; i++){
+                    if (num % i == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) System.out.println(num);
+                flag = true;
+                num++;
+            }
     }
 }
