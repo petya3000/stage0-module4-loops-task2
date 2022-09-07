a8498f2eaae97200dcb266c0ce4e3b480ef49433
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-07 18:10:08 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        for (char chr : chars){
+                System.out.print(chr);
+            }
 
     }
 }
